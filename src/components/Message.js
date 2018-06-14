70a611552d6279769c9d480efa175f859023869d
Michael Cheng <michael.cheng@flatironschool.com>
2018-06-14 14:46:28 -0400

    hideable delete button

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Message = (props) => {
-  return (
-    <div className="message">
-      <p className="text">{props.text}</p>
-    </div>
-  )
+class Message extends Component {
+  state = {
+    hideDeleteButton: true,
+  }
+
+  toggleDeleteButton = () => {
+    this.setState(prevState => {
+      return { hideDeleteButton: !prevState.hideDeleteButton }
+    });
+  }
+
+  render() {
+    const deleteButton = this.state.hideDeleteButton || <button onClick={this.deleteMe}>Delete</button>;
+
+    return (
+      <div onClick={this.toggleDeleteButton} className="message">
+        <p className="text">{this.props.text}</p>
+        { deleteButton }
+        {/* this.state.hideDeleteButton ?
+            null
+          :
+          <button onClick={this.deleteMe}>Delete</button>
+        */}
+      </div>
+    )
+  }
 }
 
 export default Message;
